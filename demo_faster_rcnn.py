57ec2d16abce07f267e43a2e23dd466da58b5e72
anhtranhoang <hoanganhvt12@gmail.com>
2020-07-07 14:50:33 +0700

    7-7

--- demo_faster_rcnn.py
+++ demo_faster_rcnn.py
@@ -11,29 +11,24 @@ import matplotlib.pyplot as plt
 import mxnet as mx
 import glob,time
 from random import shuffle
-ctx = mx.gpu(0)
-
-
-
-path  = 'F:/HoangAnh/ScanComProject/Training/results/faster_rcnn_fpn_resnet50_v1b_coco_best.params'
-net = model_zoo.get_model('faster_rcnn_fpn_resnet50_v1b_coco', pretrained=False,ctx = [ctx])
-net.classes = ['_background_','back-left leg','back-right leg','front leg','top part','not top part']
-net.load_parameters(path,allow_missing=True, ignore_extra=True,ctx = [ctx])
-# img  = 'F:/HoangAnh/ScanComProject/Code/AppDemo/debug/1592187695.9447381_0.jpg'
-img = 'debug'
-img = glob.glob(img + "/*")
-shuffle(img)
-i = 0
+from predict_faster_rcnn import predict_product
 from tqdm import tqdm
-for im in tqdm(img):
 
-    tic = time.time()
-    x, orig_img = data.transforms.presets.rcnn.load_test(im,max_size= 1333,short=800)
-    x = mx.nd.array(x,ctx = ctx)
-    # orig_img = mx.nd.array(orig_img,ctx = ctx)
-    # x, orig_img = data.transforms.presets.rcnn.transform_test()
-    box_ids, scores, bboxes = net(x)
-    ax,c = utils.viz.cv_plot_bbox(orig_img, bboxes[0], scores[0], box_ids[0], class_names=net.classes,thresh=0.4,mode = 1,debug = True)
-    # print("process time : {}".format(time.time()-tic) )
-    cv2.imwrite("img1/{}.jpg".format(i),ax)
-    i += 1
+
+if __name__ == "__main__":
+    ctx = mx.gpu(0)
+    img = 'frame1'
+    img = sorted(glob.glob(img + "/*"))
+    path  = 'F:/HoangAnh/ScanComProject/Training/weightsfaster_rcnn_fpn_resnet50_v1b_coco_best.params'
+    net = model_zoo.get_model('faster_rcnn_fpn_resnet50_v1b_coco', pretrained=False,ctx = [ctx])
+    net.classes = ['_background_','back-left leg','back-right leg','front leg','box','hardware']
+    net.load_parameters(path,allow_missing=True, ignore_extra=True,ctx = [ctx])
+    # shuffle(img)
+    
+    i = 0    
+    for im in tqdm(img):
+        tic = time.time()
+        ax,c = predict_product(net,im,ctx = ctx,mode = 1)
+        # print("time process : ", time.time()-tic)
+        cv2.imwrite("img/{s}.jpg".format(s = str(i).zfill(3)),ax)
+        i += 1
